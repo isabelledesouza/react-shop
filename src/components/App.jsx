893b7af9ed2b9715ce4fbe51f4f7d26970deee32
Isabelle De Souza <isabelle.cristine19@gmail.com>
2023-04-04 09:24:27 -0500

    Primer commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Login from "../containers/Login"
+import "../styles/global.scss"
+
+const App = () => {
+    return (
+        <div>
+            <Login/>
+        </div>
+    );
+}
+
+export default App;
